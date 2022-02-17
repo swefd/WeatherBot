1ad19e909cdb543f14ee822708815b5f3787ce93
swefd <swefd>
2022-02-17 12:02:38 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TelegramBotsApi telegramBotsApi;
+
+        try {
+            telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
